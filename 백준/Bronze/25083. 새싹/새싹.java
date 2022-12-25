2558c0abce2248d33bec05960fd9d3838258b1eb
SONSOOMIN <80534651+sonmansu@users.noreply.github.com>
2022-12-25 22:33:13 +0900

    [Bronze V] Title: 새싹, Time: 112 ms, Memory: 13836 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |";
+        System.out.println(str);
+    }
+}
