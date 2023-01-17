e25f40f2f7b65fa3fb89e27f084909491cd656aa
SONSOOMIN <80534651+sonmansu@users.noreply.github.com>
2023-01-17 18:17:56 +0900

    [Silver II] Title: 좌표 압축, Time: 1884 ms, Memory: 344720 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.js
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.js
@@ -2,13 +2,15 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let [n, arr] = fs.readFileSync(filePath).toString().replace(/\r/g, '').trim().split('\n');
 arr = arr.split(' ').map(Number);
+
 let set = new Set(arr);
 let unique = [...set];
-let map = new Map();
-let res = [];
-
 unique.sort((a, b) => a - b);
+
+let map = new Map();
 unique.forEach((val, idx) => map.set(val, idx));
+
+let res = [];
 arr.forEach(val => res.push(map.get(val)));
 
 console.log(res.join(' '));
