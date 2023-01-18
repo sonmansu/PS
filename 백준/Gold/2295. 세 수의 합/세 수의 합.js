9fcbca19dba8c4dc3794352732b98e7d96b5d268
SONSOOMIN <80534651+sonmansu@users.noreply.github.com>
2023-01-18 16:12:37 +0900

    [Gold IV] Title: 세 수의 합, Time: 352 ms, Memory: 31600 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.js
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.js
@@ -2,17 +2,6 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let [n, ...arr] = fs.readFileSync(filePath).toString().replace(/\r/g, '').trim().split('\n').map(Number);
 
-arr.sort((a, b) => a - b);
-
-let sums = [];
-
-for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-        sums.push(arr[i] + arr[j]);
-    }
-}
-sums.sort((a, b) => a - b);
-
 let binarySearch = target => {
     let st = 0;
     let en = sums.length - 1;
@@ -29,14 +18,25 @@ let binarySearch = target => {
     return -1;
 }
 
-let res = [];
+arr.sort((a, b) => a - b);
 
-for (let i = n - 1; i > 0; i--) {
-    for (let j = 0; j < i; j++) {
-        let find = binarySearch(arr[i] - arr[j]);
-        if (find >= 0)
-            res.push(arr[i]);
+let sums = [];
+
+// 두 수의 합 배열 만듦
+for (let i = 0; i < n; i++) {
+    for (let j = i; j < n; j++) { // (1) 2, 4 더해준 뒤에 4, 2 또 더 해줄 필요 없으니 j = i부터 시작하도록 변경
+        sums.push(arr[i] + arr[j]);
     }
 }
+sums.sort((a, b) => a - b);
 
-console.log(Math.max(...res));
+// a[l] - a[k]를 sums 배열에서 이분탐색으로 찾음
+for (let i = n - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+        if (binarySearch(arr[i] - arr[j]) >= 0) // 처음으로 발견한 것이 제일 크기 때문에 출력하고 종료
+        {
+            console.log(arr[i]);
+            return;
+        }
+    }
+}
